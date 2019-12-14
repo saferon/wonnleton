69df5095d9db4a4ba06608e31cfdaa9bd1db6489
saferon <55004372+saferon@users.noreply.github.com>
2019-12-14 05:00:33 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+// ideas: add translate function, integrate with l.i.n.d.a, fetch youtube uploads from mike, mark, trevor, and sonny
+
 const Discord = require("discord.js");
 const { token } = require("./config.json");
 const { wlt } = require("./WLT.json");
+const { help } = require("./helplist.json");
 var fs = require("fs");
 const bot = new Discord.Client();
-
 const imagePath = "C:\\Users\\camer\\Desktop\\sorted\\";
+const lindaList = fs.readdirSync(imagePath);
 
 bot.on("ready", () => {
   console.log("WONNLETON HAS RISEN");
+  console.log(lindaList);
+  console.log(listSort(lindaList));
 });
 
 bot.on("message", msg => {
@@ -38,10 +43,39 @@ bot.on("message", msg => {
   if (msg.content.includes("cheers")) {
     msg.channel.send("Aye. Cheers.");
   }
+  if (msg.content.includes("spicy")) {
+    msg.channel.send("ไม่เผ็ดไม่กิน");
+  }
+  if (msg.content === "!lindas") {
+    var lindaList = fs.readdirSync(imagePath);
+    var index = lindaList.indexOf("desktop.ini");
+    lindaList.splice(index, 1);
+    msg.author.send("```" + lindaList + "```");
+  }
+  if (msg.content === "!randomlinda") {
+    var lindaList = fs.readdirSync(imagePath);
+    var randomFolder = randomIndex(lindaList.length);
+    var chosenFolder = fs.readdirSync(imagePath + lindaList[randomFolder]);
+    var lindaFolder = randomIndex(chosenFolder.length);
+    msg.channel.send(lindaList[randomFolder] + "!", {
+      files: [
+        imagePath + lindaList[randomFolder] + "\\" + chosenFolder[lindaFolder]
+      ]
+    });
+  }
+  if (msg.content === "!help") {
+    msg.author.send(help);
+  }
 });
 
 function randomIndex(length) {
   return Math.floor(Math.random() * length);
+}
+
+function listSort(list) {
+  for (var i = 0; i < list.length / 10; i++) {
+    list.slice(i * 10, 10 + i * 10);
+  }
 }
 
 bot.login(token);
