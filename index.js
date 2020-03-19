512c656047db95d1f9dbe4b1c083ea35bcf229f7
saferon <cameronshandlite@gmail.com>
2020-03-19 20:51:04 +0000

    ok pls it's broken

--- index.js
+++ index.js
@@ -343,12 +343,13 @@ bot.on('message', async msg => {
         for (var i = 0; i < server.queue.length; i++) {
             ytdl.getInfo(server.queue[i], function(err, info) {
             if (err) throw err
-            var position = i
-            var positions = info.title
-            var posi = position + " - " +positions
-            console.log("in the for loop " + posi)
-            printQueue.push(posi)
-            if (i === server.queue.length - 1) {
+            var position = i;
+            var positions = info.title;
+            var posi = position + " - " + positions;
+            console.log("in the for loop " + posi);
+            printQueue.push(posi);
+            if (i === server.queue.length + 1) {
+              console.log("Sending out printQueue");
               msg.channel.send(printQueue);
             }
           });
