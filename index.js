e1f2ba5aa4575043e533c64d2ca090553d72fca6
saferon <cameronshandlite@gmail.com>
2020-03-19 02:32:22 +0000

    love jpy cheers

--- index.js
+++ index.js
@@ -291,7 +291,7 @@ bot.on('message', async msg => {
   if (command === 'play') {
     function play(connection, msg) {
       var server = queued[msg.guild.id];
-      server.dispatcher = connection.playStream(ytdl(queued[msg.guild.id].queue[0], {filter: "audioonly"}));
+      server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}));
       server.queue.shift();
       server.dispatcher.on("end", function(){
         if (server.queue[0]){
@@ -315,6 +315,24 @@ bot.on('message', async msg => {
     })
   }
 
+  if (command === 'queue') {
+    var link = args[0]
+    console.log(link)
+    if (!link || !link.includes("www.youtube.com/" || "https://youtu.be/")) return msg.reply("You need to give a youtube link to play.")
+    if (!msg.member.voiceChannel) return msg.reply("You must be in a voice channel.")
+    if (!queued[msg.guild.id]) queued[msg.guild.id] = {
+      queue: []
+    }
+    var server = queued[msg.guild.id];
+    if (!args[0]) { 
+      msg.channel.send(server.queue);
+      return;
+    } else {
+    server.queue.push(link);
+    msg.channel.send("Added to the queue!")
+    }
+  };
+
   if (command === 'skip') {
     var server = queued[msg.guild.id];
     if (server.dispatcher) server.dispatcher.end();
@@ -324,14 +342,17 @@ bot.on('message', async msg => {
   if (command === 'stop') {
     var server = queued[msg.guild.id];
     if (msg.guild.voiceChannel) {
-      for (var i = server.queue.length -1; i >=0, i--;) {
+      for (var i = server.queue.length -1; i >=0; i--) {
         server.queue.splice(i, 1);
       }
       server.dispatcher.end();
       console.log("-----------------")
       console.log("stopped the queue")
     }
-    if (msg.guild.connection) msg.guild.voiceChannel.disconnect();
+    if (msg.guild.connection){
+      console.log("disconnected")
+      msg.guild.voiceChannel.disconnect();
+    }
   }
 
 
